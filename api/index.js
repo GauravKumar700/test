45671725860d4b6aab818291ec72eaa919e302e8
Gaurav Kumar <gauravkumar7004515@gmail.com>
2025-05-20 00:55:38 +0530

    Intial commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import connectDB from '../server/database/db';
+import connectDB from '../server/database/db.js';
 
-connectDB()
 const app = express();
+connectDB()
 
 app.get('/', (req, res) => {
     res.send('Express on Vercel');
