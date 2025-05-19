69b38e08f9e59eacb4b532d86be73d1888634add
Gaurav Kumar <gauravkumar7004515@gmail.com>
2025-05-19 18:58:55 +0530

    Initial Commit4

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get('/', (req, res) => {
+    res.send('Express on Vercel');
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// ✅ Do NOT call app.listen() on Vercel
 
-module.exports = app;
+export default app;
