d86d4e1023d5d0d915f48e0d858aeab9ea112cf8
Gaurav Kumar <gauravkumar7004515@gmail.com>
2025-05-21 19:23:29 +0530

    Intial commit2

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('../db.js');
+const hello = require('../routes/hello.js')
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/hello', hello)
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
